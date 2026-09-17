628b8402c3cd1dfab411edf9870a081eabc3ee6e
Dzming Li <i@dzming.li>
2026-09-17 12:19:48 +0800

    Move org-fragtog-plus to its own repository

--- flake.nix
+++ flake.nix
@@ -25,23 +25,19 @@
             nativeBuildInputs = [ pkgs.gnutar ];
           } ''
             mkdir -p "$out"
-            for name in org-typst-math org-fragtog-plus; do
-              source=${./lisp}/$name.el
-              version=$(sed -n 's/^;; Version: //p' "$source")
-              dependencies=$(sed -n 's/^;; Package-Requires: //p' "$source")
-              directory="$name-$version"
-              mkdir "$directory"
-              cp ${./lisp}/$name*.el "$directory/"
-              if [ "$name" = org-typst-math ]; then
-                cp ${./lisp}/typst-client.el "$directory/"
-              fi
-              cat > "$directory/$name-pkg.el" <<EOF
+            name=org-typst-math
+            source=${./lisp}/$name.el
+            version=$(sed -n 's/^;; Version: //p' "$source")
+            dependencies=$(sed -n 's/^;; Package-Requires: //p' "$source")
+            directory="$name-$version"
+            mkdir "$directory"
+            cp ${./lisp}/*.el "$directory/"
+            cat > "$directory/$name-pkg.el" <<EOF
             ;;; -*- no-byte-compile: t; lexical-binding: t; -*-
             (define-package "$name" "$version" "$name" '$dependencies)
             EOF
-              tar --sort=name --mtime=@1 --owner=0 --group=0 --numeric-owner \
-                -cf "$out/$directory.tar" "$directory"
-            done
+            tar --sort=name --mtime=@1 --owner=0 --group=0 --numeric-owner \
+              -cf "$out/$directory.tar" "$directory"
           '';
         });
       devShells = forAllSystems (system:
